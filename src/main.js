82bd513e3818787712bc703c47ffcc3683383565
Dominik Krebs <dominik.krebs@netzkollektiv.com>
2021-04-30 12:26:41 +0200

    shadow dom first steps

--- src/main.js
+++ src/main.js
@@ -15,4 +15,16 @@ export const bus = new Vue()
 Vue.config.productionTip = false
 
 Vue.customElement('easycredit-checkout-label', CheckoutLabel)
-Vue.customElement('easycredit-checkout', Checkout)
+Vue.customElement('easycredit-checkout', Checkout, {
+    shadow: true,
+    beforeCreateVueInstance(root) {
+      const rootNode = root.el.getRootNode();
+  
+      if (rootNode instanceof ShadowRoot) {
+        root.shadowRoot = rootNode;
+      } else {
+        root.shadowRoot = document.head;
+      }
+      return root;
+    },
+  })
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,44 @@
+function enableShadowCss(config) {
+  const configs = [
+    // Required to explicitly call the __inject__ function exported by
+    // vue-style-loader on vue mounting
+    // (though the shadow DOM should be created and marked
+    //  first by the main js or the style injection will fail)
+    config.module.rule('vue').use('vue-loader'),
+  ];
+  const ruleSets = ['css', 'postcss', 'scss', 'sass', 'less', 'stylus'];
+  const ruleNames = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+  // console.info('**** config.module.rules **** ');
+  // console.info(config.module.rules.store.keys());
+
+  ruleSets.forEach((ruleSet) => {
+    if (config.module.rules.store.has(ruleSet)) {
+      // console.info(`**** config.module.rule(${ruleSet}) names: `);
+      // console.info(config.module.rule(ruleSet).oneOfs.store.keys());
+
+      ruleNames.forEach((rName) => {
+        if (config.module.rule(ruleSet).oneOfs.store.has(rName)) {
+          // console.info(`**** config.module.rule(${ruleSet}).oneOf(${rName}) uses: `);
+          // console.info(config.module.rule(ruleSet).oneOf(rName).uses.store.keys());
+          if (config.module.rule(ruleSet).oneOf(rName).uses.store.has('vue-style-loader')) {
+            // console.info(`*** vue-style-loader found in rule(${ruleSet}).oneOf(${rName})!`);
+            configs.push(config.module.rule(ruleSet).oneOf(rName).use('vue-style-loader'));
+          }
+        }
+      });
+    }
+  });
+  if (!process.env.BUILD_MODE) {
+    process.env.BUILD_MODE = config.store.get('mode');
+  }
+  configs.forEach((c) => c.tap((options) => {
+    // eslint-disable-next-line no-param-reassign
+    console.log('shawdow!');
+    options.shadowMode = true;
+    return options;
+  }));
+}
+
 const webpack = require('webpack')
 module.exports = {
   configureWebpack: {
@@ -5,18 +46,16 @@ module.exports = {
       new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1
       })
-    ],
-//    output: {
-//        filename: "easycredit-checkout.js"
-//    }
+    ]
   },
   filenameHashing: false,
   chainWebpack:
     config => {
       config.optimization.delete('splitChunks')
-    },
-  devServer: {
-      host: 'docker03.netzkollektiv.com',
-      port: '8080'
+      enableShadowCss(config)
   },
+  css: {
+    sourceMap: true,
+    extract: false
+  }
 }
